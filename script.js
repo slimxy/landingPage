96f5c98ecbcf3c15a8c8614104f60b31edc39211
slimxy <tasiusalim260@gmail.com>
2023-01-26 11:13:17 +0100

    digital agency

---
+++ script.js
@@ -0,0 +1,7 @@
+ let bar= document.querySelector(".fa-bars");
+ let tab= document.querySelector(".links-tab");
+// let myNav= document.querySelector(".nav");
+
+function show(){
+    tab.classList.toggle("active")
+}
